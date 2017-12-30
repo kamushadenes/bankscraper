86e7fa6b42e30b40caac2300c09654ab8c58e690
Adorilson Bezerra <adorilson@gmail.com>
2017-12-30 18:52:33 -0300

    BancoDoBrasilValidator is more flexivel about branch and account number
    To this validator, branch_size and account_size works as max size, because
    branch and account numbers don't has fixed width

--- bankscraper/validators.py
+++ bankscraper/validators.py
@@ -161,6 +161,8 @@ class BankValidator(BaseValidator):
 
 class BancoDoBrasilValidator(BankValidator):
 
+    # To this validator, the two next values works as max size,
+    # because branch and account numbers don't has fixed width
     branch_size = 5
     account_size = 6
     password_size = 8
@@ -168,12 +170,21 @@ class BancoDoBrasilValidator(BankValidator):
     allowed_days = [30]
 
     fields = ['branch', 'number', 'password']
-    
+
     def branch(self, branch):
-        if len(branch) != self.branch_size:
+        return len(branch) <= self.branch_size
+
+    def account(self, account):
+
+        if '-' not in account:
+            try:
+                int(account)
+            except ValueError:
+                return False
+        else:
             return False
 
-        return True
+        return len(account) <= self.account_size
 
 
 class ItauValidator(BankValidator):
